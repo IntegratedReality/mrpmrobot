4dbbab25c69a3f1a9b9eaeb384a64aeb05781d96
Tarou Seimitsu <pe2015mayfestival@gmail.com>
2016-02-27 06:30:09 +0000

    それっぽく動いたが、StopでなぜかStopしない

--- motor/test_motor.cpp
+++ motor/test_motor.cpp
@@ -10,13 +10,27 @@ int main(void)
     motor.at(0).init(19, 26, true);
     motor.at(1).init(20, 21, true);
     int counter = 0;
+    bool downcounterflag = false;
+    bool next_end_flag = false;
     while (true) {
         motor.at(0).setMotor(MotorMode::Move, counter / 1000.0);
         std::cout << counter << std::endl;
-        counter++;
+        if (!downcounterflag) {
+            counter++;
+        } else {
+            counter--;
+        }
         if (counter > 1000) {
+            downcounterflag = true;
+        } 
+        if (downcounterflag && counter < -1000) {
+            next_end_flag = true;
+            downcounterflag = false;
+        }
+        if (next_end_flag && counter == 1) {
             break;
         }
+        delay(10);
     }
     return 0;
 }
