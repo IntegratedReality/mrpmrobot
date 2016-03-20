583d4139358b76ceb6aac3c92aaea11d4b683d59
Tarou Seimitsu <pe2015mayfestival@gmail.com>
2016-03-20 22:44:55 +0000

    create all file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+Robot's Main ProgramA of MRPM Project
+Dept. of Precision Engneering, U-tokyo
+Seimitsu Lab, Mayfes
+*/
+
+#include <iostream>
+#include <array>
+
+#include "motor/motor.h"
+#include "motor/drive.h"
+#include "motor/operate.h"
+
+
+void main(void)
+{
+
+
+    return 0;
+}
+
