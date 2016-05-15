5b54ad317167d786ace179cd9f38a428c23d49e1
Taro Seimitsu <pe2015mayfestival@gmail.com>
2016-05-15 04:00:25 +0000

    バグを殺した

--- main.cpp
+++ main.cpp
@@ -80,8 +80,9 @@ int main(int argc, char **argv)
 
 	long count = 0;
 	std::thread ai_thread([&](){
-		static bool last_shot_state = false;
+		sender.sendShot(ID, false);
 		while (ID >= 3 || receiver.getData(ID).isAI) {
+			static bool last_shot_state = ai.getOperation().shot;;
 			if (!(receiver.getData(ID).state == DEAD || receiver.getData(ID).state == STANDBY)) {
 				if (last_shot_state != ai.getOperation().shot) {
 					sender.sendShot(ID, ai.getOperation().shot);
